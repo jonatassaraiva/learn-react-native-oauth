afd1e69e6241ce053111c8b1866b00154df0e4ef
Jonatas Saraiva <jonatas.saraiva@globalhitss.com.br>
2017-03-04 23:57:12 -0300

    Add component header

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 
+import { Header } from './component/common';
+
 class App extends Component {
 
   render() {
     return (
-      <View/>
+      <View>
+        <Header text="Authetication" />
+      </View>
     );
   }
 }
