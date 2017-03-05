7e18a5225ed1262370e36b30eaa2dbc3803a1e8e
Jonatas Saraiva <jonatas.saraiva@globalhitss.com.br>
2017-03-05 00:15:15 -0300

    Add firebase and settings

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import firebase from 'firebase';
 
 import { Header } from './component/common';
+import { firebaseSetting } from '../setting';
 
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp(firebaseSetting);
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user)
+        this.setState({ loggedIn: true });
+      else
+        this.setState({ loggedIn: false });
+    });
+  }
 
   render() {
     return (
